1faffa55a5fe4f98cb7c12613d33990b754d81fa
Ronald Arias <ronald.arias@thoughtworks.com>
2021-04-03 21:16:37 -0500

    Refactors the code to create a new NumbersCollection with compare and swap methods

--- test/Sorter.test.ts
+++ test/Sorter.test.ts
@@ -1,11 +1,13 @@
+import { NumbersCollection } from '../src/NumbersCollection';
 import { Sorter } from '../src/Sorter';
 
 describe('Sorting tests', () => {
   it('Sort an array of numbers in ascedent order', () => {
-    const sorter = new Sorter([10, 2, -4, 0]);
+    const numbersCollection = new NumbersCollection([10, 2, -4, 0]);
+    const sorter = new Sorter(numbersCollection);
 
     sorter.sort();
 
-    expect(sorter.collection).toStrictEqual([-4, 0, 2, 10]);
+    expect(sorter.collection.data).toStrictEqual([-4, 0, 2, 10]);
   });
 });
